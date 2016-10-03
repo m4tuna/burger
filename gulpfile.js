d1be8d58d0394e042c6730fc438fea1c9a221a59
Mike Fortuna <m4tuna@gmail.com>
2016-10-03 15:53:40 -0400

    Updated components, added elements, updated vars.

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,42 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('sass', function () {
-  return gulp.src('./scss/**/*.scss')
-    .pipe(sass.sync().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+  browserSync.init({
+    server: "./"
+  });
+
+  gulp.watch("./scss/*.scss", ['sass']);
+  gulp.watch("./*.html").on('change', browserSync.reload);
 });
+
+/**
+* Compile with gulp-ruby-sass + source maps
+*/
+gulp.task('sass', function () {
+
+  return sass('./scss', {sourcemap: true})
+  .on('error', function (err) {
+    console.error('Error!', err.message);
+  })
+  .pipe(sourcemaps.write('./', {
+    includeContent: false,
+    sourceRoot: '/app/scss'
+  }))
+  .pipe(browserSync.stream({match: '**/*.css'}));
+});
+//
+//
+// gulp.task('sass', function () {
+//   return gulp.src('./scss/**/*.scss')
+//     .pipe(sass.sync().on('error', sass.logError))
+//     .pipe(gulp.dest('./css'));
+// });
 
 gulp.task('watch', function () {
   gulp.watch('./scss/**/*.scss', ['sass']);
