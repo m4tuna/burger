9f098e54829e83ce2d411670b5a3ad234bd5c494
Mike Fortuna <m4tuna@gmail.com>
2016-10-14 21:59:02 -0400

    Modal v1

--- burger.js
+++ burger.js
@@ -1,13 +1,51 @@
-var menuButton = document.getElementById('menuButton');
-menuButton.addEventListener('click', function (e) {
-  menuButton.classList.toggle('is-active');
+// Menu button
+$("#menuButton").click(function(e) {
+  $( this ).toggleClass('is-active');
   e.preventDefault();
 });
+// Card
 $("#openClose").change(function() {
   $( ".card" ).toggleClass('collapsed');
 });
-
+// Loading
 $("#loading").change(function() {
   $( ".loading-bar" ).toggleClass('hide');
-  $(this).parent().toggleClass('relative');
+});
+
+// Modal
+var openModal = function() {
+  $( "body" ).addClass('modal-open');
+}
+
+var closeModal = function() {
+  $( "body" ).removeClass('modal-open');
+}
+
+$("#openModal").click(function(e) {
+  openModal();
+  e.preventDefault();
+});
+
+$("#closeModal").click(function(e) {
+  closeModal();
+  e.preventDefault();
+});
+
+$(".overlay").click(function(e) {
+  closeModal();
+  e.preventDefault();
+});
+// Modal
+
+
+$( document ).ready(function() {
+  // Doc ready
+  console.log( "ready!" );
+});
+
+$(document).keyup(function(e) {
+  // Keyup
+  if (e.keyCode == 27) { // escape key maps to keycode `27`
+    closeModal();
+  }
 });
